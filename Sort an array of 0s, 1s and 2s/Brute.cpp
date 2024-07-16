065322c8c5f7a1694445d83669b3d381147df5be
shilpa4103 <shilpashree4103@gmail.com>
2024-07-16 18:26:02 +0530

    Brute approach for Sorting an array

---
+++ Sort an array of 0s, 1s and 2s/Brute.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int>arr={0,1,2,1,0,0,1,2,2};
+    int n=arr.size();
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" "; 
+    }
+    return 0;
+}
