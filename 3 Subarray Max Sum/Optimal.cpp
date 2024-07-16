6fa3bed229212b31a0c9d77054082cc158b0e2c1
shilpa4103 <shilpashree4103@gmail.com>
2024-07-16 20:39:44 +0530

    Done

---
+++ 3 Subarray Max Sum/Optimal.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int maxSubarraysum(vector<int> arr,int n){
+    int maxi=INT_MIN;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+        maxi=max(sum,maxi);
+        if(sum<0){
+            sum=0;
+        }
+    }
+    return maxi;
+}
+int main(){
+    vector<int>arr={ -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n=arr.size();
+    int sum=maxSubarraysum(arr,n);
+    cout<<sum;
+    return 0;
+}
