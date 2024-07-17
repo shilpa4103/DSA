1b68c9125ca16cb4a6709e78ee84671c9f3a929d
shilpa4103 <shilpashree4103@gmail.com>
2024-07-17 17:29:52 +0530

    Done

---
+++ 8 Duplicate in an array/Brute.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findDuplicate(vector<int>&arr){
+    int n=arr.size();
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<n;i++){
+        if(arr[i]==arr[i+1]){
+            return arr[i];
+        }
+    }
+}
+
+int main(){
+    vector<int>arr={4,3,1,2,1};
+    int ans=findDuplicate(arr);
+    cout<<ans;
+    return 0;
+}
+
+//Time complexity : O(nlogn)+O(n)
+//Space complexity : O(1)
