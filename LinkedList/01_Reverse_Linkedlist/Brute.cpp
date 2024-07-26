2926bc5eb2623605527efa58a537aa647948675d
shilpa4103 <shilpashree4103@gmail.com>
2024-07-26 14:56:05 +0530

    Done

---
+++ LinkedList/01_Reverse_Linkedlist/Brute.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+
+};
+
+
+Node* reverseLinkedList(Node * head){
+    Node* temp=head;
+    stack<int>st;
+    while(temp!=NULL){
+        st.push(temp->data);
+        temp=temp->next;
+    }
+    temp=head;
+    while(temp!=NULL){
+        temp->data=st.top();
+        st.pop();
+        temp=temp->next;
+    }
+    return head;
+    }
+
+
+void printLinkedList(Node* head){
+    Node * temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    Node* head=new Node(1);
+    head->next=new Node(2);
+    head->next->next=new Node(3);
+    head->next->next->next=new Node(4);
+
+    cout << "Original Linked List: ";
+    printLinkedList(head);
+
+    head=reverseLinkedList(head);
+    cout << "Reversed Linked List: ";
+    printLinkedList(head);
+
+    return 0;
+}
