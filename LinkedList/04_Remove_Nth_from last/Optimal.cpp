bebfb80bedcc2f26a915c124509e078fabac63f3
shilpa4103 <shilpashree4103@gmail.com>
2024-07-27 23:39:40 +0530

    Done

---
+++ LinkedList/04_Remove_Nth_from last/Optimal.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data,Node* next){
+        this->data=data;
+        this->next=next;
+    }
+
+    Node(int data){
+        this->data=data;
+        this->next=nullptr;
+    }
+};
+
+Node* removeNth(Node* head,int n){
+    // Create two pointers, fastp and slowp
+    Node* fastp = head;
+    Node* slowp = head;
+
+    // Move the fastp pointer N nodes ahead
+    for (int i = 0; i < N; i++)
+        fastp = fastp->next;
+
+    // If fastp becomes NULL,
+    // the Nth node from the end is the head
+    if (fastp == NULL)
+        return head->next;
+
+    // Move both pointers until fastp reaches the end
+    while (fastp->next != NULL) {
+        fastp = fastp->next;
+        slowp = slowp->next;
+    }
+
+    // Delete the Nth node from the end
+    Node* delNode = slowp->next;
+    slowp->next = slowp->next->next;
+    delete delNode;
+    return head;
+
+}
+
+void printLinkedList(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+int main(){
+    Node *head=new Node(1);
+    head->next=new Node(2);
+    head->next->next=new Node(3);
+    int n;
+    cin>>n;
+
+    head=removeNth(head,n);
+    printLinkedList(head);
+
+    return 0;
+}
---
+++ LinkedList/04_Remove_Nth_from last/brute.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data,Node* next){
+        this->data=data;
+        this->next=next;
+    }
+
+    Node(int data){
+        this->data=data;
+        this->next=nullptr;
+    }
+};
+
+Node* removeNth(Node* head,int n){
+    Node *temp=head;
+    int cnt=0;
+    while(temp!=NULL&&temp->next!=NULL){
+        cnt=cnt+2;
+        temp=temp->next->next;
+    }
+    if(temp!=NULL){
+        cnt=cnt+1;
+    }
+    int m=cnt-n;
+    temp=head;
+    Node*slow=NULL;
+    for(int i=0;i<m;i++){
+        slow=temp;
+        temp=temp->next;
+    }
+    slow->next=temp->next;
+    free(temp);
+    return head;
+
+}
+
+void printLinkedList(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+int main(){
+    Node *head=new Node(1);
+    head->next=new Node(2);
+    head->next->next=new Node(3);
+    int n;
+    cin>>n;
+
+    head=removeNth(head,n);
+    printLinkedList(head);
+
+    return 0;
+}
